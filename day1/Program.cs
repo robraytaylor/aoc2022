61215b3dfea5945884fe0ef870df9cff8fa243cb
rob t <bobt_37@hotmail.com>
2022-12-01 08:51:25 +0000

    Day 1 Solution

---
+++ day1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
+var inputLines = File.ReadAllLines("puzzle_input.txt");
+
+var elfCalories = new List<int>();
+var calorieCounter = 0;
+
+foreach (var line in inputLines)
+{
+    if(string.IsNullOrWhiteSpace(line))
+    {
+        elfCalories.Add(calorieCounter);
+        calorieCounter = 0;
+    }
+    else
+    {
+        calorieCounter += int.Parse(line);
+    }
+}
+
+Console.WriteLine($"Highest Elf Calories = {elfCalories.Max()}");
+
+Console.WriteLine($"Top 3 Elf Total Calories = {elfCalories.OrderByDescending(x => x).Take(3).Sum()}");
