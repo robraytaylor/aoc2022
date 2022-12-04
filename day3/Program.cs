1e60eff945e3dc249e27aa313c1e6fa6a7aafdc0
rob t <bobt_37@hotmail.com>
2022-12-04 09:13:00 +0000

    Day 3 and 4

---
+++ day3/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char GetMatchedItem(string rucksack)
+{
+    var middle = rucksack.Length /2;
+    var one = rucksack.Substring(0,middle);
+    var two = rucksack.Substring(middle, middle);
+
+    char matched = '1';
+    foreach (var c in one)
+    {
+        foreach (var c2 in two)
+        {
+            if(c == c2)
+            {
+                matched = c;
+                break;
+            }
+        }
+        if(matched != '1')
+            break;
+    }
+    return matched;
+}
+
+int GetPriority(char c)
+{
+    // ascii a = 97, ascii A = 65
+    if((int)c > 96)
+        return (int)c - 96;
+    else
+        return (int)c - 64 + 26;
+}
+
+var input = File.ReadAllLines("puzzle_input.txt");
+
+Console.WriteLine($"Priority Sum = {input.Sum(x => GetPriority(GetMatchedItem(x)))}");
+
+var total = 0;
+
+while(input.Any())
+{
+    var group = input.Take(3);
+    input = input.Skip(3).ToArray();
+
+    var distinctConcat = string.Join(string.Empty, group.Select(x => new string(x.Distinct().ToArray())));
+    var badge = distinctConcat.GroupBy(x => x).Where(x => x.Count() == 3).First().Key;
+    total += GetPriority(badge);
+}
+
+Console.WriteLine($"Badge Priority Sum = {total}");
---
+++ day4/Program.cs
@@ -0,0 +1,25 @@
+﻿var lines = File.ReadAllLines("puzzle_input.txt");
+
+(int Start, int End) GetRange(string range)
+{
+    var split = range.Split("-");
+    return new (int.Parse(split[0]),int.Parse(split[1]));
+}
+
+
+var parsedData = lines
+    .Select(x => x.Split(","))
+    .Select(x => new {One = GetRange(x[0]), Two = GetRange(x[1])});
+
+var completeOverlap = parsedData
+    .Where(x => (x.One.Start <= x.Two.Start && x.One.End >= x.Two.End)
+    || (x.Two.Start <= x.One.Start && x.Two.End >= x.One.End) );
+
+var partialOverlap = parsedData
+    .Where(x => (x.One.Start <= x.Two.Start && x.One.End >= x.Two.Start)
+    || (x.One.Start >= x.Two.Start && x.One.End <= x.Two.Start)
+    || (x.Two.Start <= x.One.Start && x.Two.End >= x.One.Start)
+    || (x.Two.Start >= x.One.Start && x.Two.End <= x.One.Start));
+
+Console.WriteLine($"Part 1 Result = {completeOverlap.Count()}");
+Console.WriteLine($"Part 2 Result = {partialOverlap.Count()}");
